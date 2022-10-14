130130e67241ee0cef42ea996ff4c5ad14e8b128
Janio <janiojunior@gmail.com>
2022-10-14 13:25:35 -0300

    Primeiro Commit

---
+++ src/blah/Teste.java
@@ -0,0 +1,13 @@
+package blah;
+
+import javax.swing.JOptionPane;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		System.out.println("Teste java 17");
+		JOptionPane.showMessageDialog(null, "Teste java 17");
+
+	}
+
+}
